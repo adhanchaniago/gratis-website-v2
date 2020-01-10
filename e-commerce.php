8379f41333dcdb3ea20ae8d03e9172fc93f2ad14
increase21 <engr.increase@gmail.com>
2020-01-10 23:34:26 +0100

    rename e-commerce

---
+++ e-commerce.php
@@ -0,0 +1,215 @@
+<?php
+include 'header.php';
+?>
+<section class="page_breadcrumbs ls section_padding_40">
+	<div class="container">
+		<div class="row">
+            <div class="col-sm-12 text-center breadcrumbs_inner">
+                <h2 class="breadcrumbs-title">
+					E-commerce                </h2>
+					<ol class="breadcrumb">
+									<li class="first-item">
+									<a href="index.html">Home</a></li>
+																<li class="0-item">
+									<a href="services-cat/our-services-2/index.html">Our Services 2</a></li>
+																<li class="last-item">E-commerce</li>
+						</ol>
+            </div>
+		</div>
+	</div>
+</section>
+<div id="content" class="single-service">
+					<article id="post-6785" class="post-6785 fw-services type-fw-services status-publish has-post-thumbnail hentry fw-services-category-our-services-2">
+				<!-- .entry-header -->
+				<div class="vertical-item">
+					<div class="item-content entry-content">
+						<div class="fw-page-builder-content"><section  id="top-banner" class="fw-main-row section  ls section_padding_top_50 section_padding_bottom_50 columns_padding_15  " style="background-image:url(<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/affilated_banner-2.jpg);">
+    <h3 class="hidden"> banner_section</h3>
+    <div class="top_corner_body"></div>
+	    <div class="container">
+        <div class="row">
+			<div  id="banner-wrap" class="col-xs-12 col-md-8 fw-column"  >
+	<div class="special-heading text-left">
+	<h2 class="section_header margin_0  ">
+	<span class=" medium text-uppercase">
+		What is	</span>
+	</h2>
+</div>
+<div class="special-heading text-left">
+	<h3 class="section_header margin_0  ">
+	<span class=" medium text-uppercase">
+		E-Commerce?	</span>
+	</h3>
+</div>
+<div class="text-block shortcode text-wrap">
+	<p>Ecommerce (electronic commerce), is the practice of using promotional tactics to drive traffic to your online store. Also, converting that traffic into paying customers and retaining those customers post-purchase attitudes. Furthermore Ecommerce marketing can promote your online store as a whole or to drive sales for specific products. It has a lot to do with brainstorming actionable tactics. Nearly every marketing operation now take place online.</p><p>This can make it resillent to discern between the various types of digital marketing people used today. Ecommerce websites can use all the digital channels to promote products and to grow a business effectively.</p></div>
+</div><div  id="column-696c200" class="col-xs-12 col-md-4 fw-column"  >
+	<div class="text-block shortcode ">
+	<div role="form" class="wpcf7" id="wpcf7-f8026-p6785-o1" lang="en-US" dir="ltr">
+<div class="screen-reader-response"></div>
+<?php include 'form.php'; ?>
+</div></div>
+</div>        </div>
+    </div>
+    <div class="bottom_corner_body"></div>
+</section><section  id="benefit-sec-wrap" class="fw-main-row section  ls ms section_padding_top_50 section_padding_bottom_50 columns_padding_15 fullwidth-section  " >
+    <h3 class="hidden"> Benefit-Section</h3>
+    <div class="top_corner_body"></div>
+	    <div class="container-fluid">
+        <div class="row">
+			<div  id="column-f88a297" class="col-xs-12 fw-column"  >
+	    <div class="padding_10">
+		<div class="text-block shortcode ">
+	<p><iframe src="https://www.youtube.com/embed/rgSo5Q2Uw4k" width="100%" height="450" frameborder="0" allowfullscreen="allowfullscreen"></iframe></p></div>
+    </div>
+</div>        </div>
+    </div>
+    <div class="bottom_corner_body"></div>
+</section><section  id="benefit-sec-wrap" class="fw-main-row section  ls section_padding_top_50 section_padding_bottom_30 columns_padding_15 fullwidth-section  " >
+    <h3 class="hidden"> Benefit-Section</h3>
+    <div class="top_corner_body"></div>
+	    <div class="container-fluid">
+        <div class="row">
+			<div  id="column-ecb0682" class="col-xs-12 col-md-6 fw-column"  >
+	<div class="img-wrap"><img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/ecommer-marketing.jpg" alt="//gratisdigital.com/<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/ecommer-marketing.jpg" /></div></div><div  id="column-c5a0065" class="col-xs-12 col-md-6 fw-column"  >
+	<div class="special-heading text-left">
+	<h3 class="section_header margin_0 heading-wrapper ">
+	<span class=" medium text-capitalize">
+		Benefits of Ecommerce Marketing	</span>
+	</h3>
+</div>
+<div class="text-block shortcode ">
+	<p>    1. The financial costs of starting an ecommerce is relatively low. In contrast, visible retail stores pay thousands of cash to obtain a rented store in the best locations. Other subsequent costs might include; store rebranding, salaries paid to staffs and for marketing campaigns. Unlike the online stores that requires no physical presence and staffs can work remotely.<br />    2. E-commerce marketing comes with a personalized online experience. Therefore, personalize your website to maximize online shopping experience. Even more, various landing pages created to suit the needs of your different customers.<br />    3. The financial cost of employing staffs is way more affordable. As a result, the ability to employ a variety of staffs with different background and geographical locations. This makes for a great spice in the business and can harness potentially growth and development ideas.<br />    4. The rate at which income streams from an e-commerce business is higher than other forms. For the reason that online stores are always open to 24/7 for business. Hence, customers of all types, lifestyle and work schedules are attracted to your business. Finally, to ease of purchasing habits.<br />    5. Anyone around the world can click on your website link and make a purchase from the comfort of their homes. Therefore, e-commerce gives a business the platform to sell internationally.</p></div>
+</div>        </div>
+    </div>
+    <div class="bottom_corner_body"></div>
+</section><section  id="brand-section" class="fw-main-row section  ls section_padding_top_50 section_padding_bottom_50 columns_padding_15 horizontal-paddings-0  " >
+    <h3 class="hidden"> Brand Section</h3>
+    <div class="top_corner_body"></div>
+	    <div class="container">
+        <div class="row">
+			<div  id="column-cbc1821" class="col-xs-12 col-md-2 fw-column"  >
+	</div><div  id="column-cf6efe1" class="col-xs-12 col-md-8 fw-column"  >
+	<div class="special-heading text-center">
+	<h3 class="section_header margin_0 heading-wrapper ">
+	<span class=" medium text-capitalize">
+		Brand that trust us 	</span>
+	</h3>
+</div>
+<div class="shortcode-image-carousel owl-carousel"
+     data-items="5"
+     data-loop="true"
+     data-nav="false"
+     data-dots="true"
+     data-center="false"
+     data-autoplay="true"
+     data-autoplaytimeout="1000"
+     data-responsive-lg="5"
+     data-responsive-md="5"
+     data-responsive-sm="3"
+     data-responsive-xs="2"
+     data-margin="30"
+>
+			<div>
+							<img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/img_1.png"
+				     alt="">
+						</div>
+				<div>
+							<img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/img_2.png"
+				     alt="">
+						</div>
+				<div>
+							<img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/img_3.png"
+				     alt="">
+						</div>
+				<div>
+							<img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/img_4.png"
+				     alt="">
+						</div>
+				<div>
+							<img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/img_5.png"
+				     alt="">
+						</div>
+				<div>
+							<img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/img_6.png"
+				     alt="">
+						</div>
+				<div>
+							<img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/img_7.png"
+				     alt="">
+						</div>
+				<div>
+							<img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/img_8.png"
+				     alt="">
+						</div>
+				<div>
+							<img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/img_9.png"
+				     alt="">
+						</div>
+				<div>
+							<img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/img_10.png"
+				     alt="">
+						</div>
+				<div>
+							<img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/img_11.png"
+				     alt="">
+						</div>
+				<div>
+							<img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/img_12.png"
+				     alt="">
+						</div>
+				<div>
+							<img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/img_13.png"
+				     alt="">
+						</div>
+		</div>
+</div><div  id="column-fdf82ec" class="col-xs-12 col-md-2 fw-column"  >
+	</div>        </div>
+    </div>
+    <div class="bottom_corner_body"></div>
+</section><section  id="#benefit-sec-wrap" class="fw-main-row section  ls section_padding_top_50 section_padding_bottom_30 columns_padding_15 fullwidth-section  " >
+    <h3 class="hidden">5de15269548d9</h3>
+    <div class="top_corner_body"></div>
+	    <div class="container-fluid">
+        <div class="row">
+			<div  id="column-d7264ae" class="col-xs-12 col-md-6 fw-column"  >
+	<div class="special-heading text-left">
+	<h3 class="section_header margin_0 heading-wrapper ">
+	<span class=" medium text-capitalize">
+		Gratis' Role	</span>
+	</h3>
+</div>
+<div class="text-block shortcode ">
+	<p>The Gratis online team also make use of the business-to-business (b2b) tatics. Above all, to attract potential customers to your online store, grow your customer base and close sales.</p></div>
+</div><div  id="column-d0b69ae" class="col-xs-12 col-md-6 fw-column"  >
+	<div class="img-wrap"><img src="<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/manage_img.jpg" alt="//gratisdigital.com/<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/manage_img.jpg" /></div></div>        </div>
+    </div>
+    <div class="bottom_corner_body"></div>
+</section><section  id="footer-call-to-action" class="fw-main-row section  ds section_padding_top_50 section_padding_bottom_50 columns_padding_15 fullwidth-section background_cover  " style="background-image:url(<?php bloginfo('template_url'); ?>/assets/uploads/2018/06/bg_adver.jpg);">
+    <h3 class="hidden"> foter-call-toaction</h3>
+    <div class="top_corner_body"></div>
+	    <div class="container-fluid">
+        <div class="row">
+			<div  id="column-00dc7ae" class="col-xs-12 fw-column"  >
+	<div class="fw-theme-call-to-action">
+			<h6 class="section_header">Hire GRATISDIGITAL to improve your ecommerce service.</h6>
+		<div class="fw-action-wrap">
+		<div class="fw-action-desc"></div>
+		<div class="fw-action-btn">
+			<a href="#header" target="_self"
+   class="wide_button theme_button color color1 large_height">
+	Get  A Free Quote</a>
+		</div>
+	</div>
+</div></div>        </div>
+    </div>
+    <div class="bottom_corner_body"></div>
+</section></div>
+					</div>
+					<!-- .entry-content -->
+				</div>
+				<!-- .entry-content -->
+			</article><!-- #post-## -->
+			</div><!--eof #content -->
+<?php
+include 'footer.php'; ?>
